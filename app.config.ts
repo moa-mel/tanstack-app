de203c38ad5fafdf404dae647ab7f7da685c7729
Olaitan <olaitanmaryakano@gmail.com>
2025-04-11 06:32:06 +0100

    new update

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from '@tanstack/react-start/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-  ],
-  server: {
-    allowedHosts: ['tanstack-app-0fvh.onrender.com'],
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+      tailwindcss(),
+    ],
   },
-});
+  server: {
+    preset: 'vercel',
+    allowedHosts: true,
+  },
+})
